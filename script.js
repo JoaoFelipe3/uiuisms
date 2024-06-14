829f19f58ce30beeee075455775533a233e71693
JoaoFelipe3 <109555243+JoaoFelipe3@users.noreply.github.com>
2024-06-14 11:18:09 -0300

    Initial commit with basic website

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    function adjustContainerPadding() {
+        const navbar = document.getElementById("navbar");
+        const container = document.getElementById("container");
+
+        container.style.paddingTop = navbar.offsetHeight + "px";
+    }
+
+    adjustContainerPadding();
+
+    window.addEventListener("resize", adjustContainerPadding);
+
+    // add pad links
+    for (let entry of document.getElementsByClassName("entry")) {
+        const code = entry.getElementsByClassName("code")[0].innerHTML;
+        let bytes = [].slice.call(new TextEncoder().encode(code));
+        bytes = bytes.map(byte => String.fromCodePoint(byte)).join("");
+
+        const td = document.createElement("td");
+        const link = document.createElement("a");
+        link.innerHTML = "Pad";
+        link.setAttribute("href", "https://uiua.org/pad?src=" + btoa(bytes));
+        td.appendChild(link);
+        entry.appendChild(td);
+    }
+});
