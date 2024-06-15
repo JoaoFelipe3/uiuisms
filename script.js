ef53331dd981527bcf7e35e81a72ad400e3825eb
JoaoFelipe3 <ccristiany339@gmail.com>
2024-06-15 08:31:40 -0300

    Revert "Merge pull request #1 from Jacob-Lockwood/master"
    This reverts commit e3a4a57b44b4a19cfd70a62d48630ddab6e964ed, reversing
    changes made to 03c9d1bb94d80a1059c64f3b9be5692461b4d905.

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // add pad links
-  for (let entry of document.getElementsByClassName("entry")) {
-    const code = entry.querySelector(".code").innerHTML;
-    let bytes = [].slice.call(new TextEncoder().encode(code));
-    bytes = bytes.map((byte) => String.fromCodePoint(byte)).join("");
+    function adjustContainerPadding() {
+        const navbar = document.getElementById("navbar");
+        const container = document.getElementById("container");
 
-    const td = document.createElement("td");
-    const link = document.createElement("a");
-    link.innerHTML = "Pad";
-    link.setAttribute(
-      "href",
-      "https://uiua.org/pad?src=" +
-        btoa(bytes).replace(/\+/g, "-").replace(/\//g, "_")
-    );
-    link.className = "underline text-teal-800 dark:text-teal-100";
-    td.appendChild(link);
-    entry.appendChild(td);
-  }
-
-  const searchBar = document.getElementById("search-bar");
-  searchBar.addEventListener("input", function () {
-    const input = searchBar.value;
-    console.log(input);
-    for (let entry of document.getElementsByClassName("entry")) {
-      const name = entry.querySelector(".name").innerHTML;
-      entry.className =
-        "entry" +
-        (name.toLowerCase().includes(input.toLowerCase()) ? "" : " hidden");
+        container.style.paddingTop = navbar.offsetHeight + "px";
     }
-  });
+
+    adjustContainerPadding();
+
+    window.addEventListener("resize", adjustContainerPadding);
+
+    // add pad links
+    for (let entry of document.getElementsByClassName("entry")) {
+        const code = entry.querySelector(".code").innerHTML;
+        let bytes = [].slice.call(new TextEncoder().encode(code));
+        bytes = bytes.map(byte => String.fromCodePoint(byte)).join("");
+
+        const td = document.createElement("td");
+        const link = document.createElement("a");
+        link.innerHTML = "Pad";
+        link.setAttribute("href", "https://uiua.org/pad?src=" + btoa(bytes).replace(/\+/g, "-").replace(/\//g, "_"));
+        td.appendChild(link);
+        entry.appendChild(td);
+    }
+
+    const searchBar = document.getElementById("search-bar");
+    searchBar.addEventListener("input", function () {
+        const input = searchBar.value;
+        console.log(input);
+        for (let entry of document.getElementsByClassName("entry")) {
+            const name = entry.querySelector(".name").innerHTML;
+            entry.className = "entry" + (name.toLowerCase().includes(input.toLowerCase()) ? "" : " invisible");
+        }
+    });
 });
