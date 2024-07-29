776e59eb40c8a9685fa39f9435f9aecee20e5e9e
JoaoFelipe3 <ccristiany339@gmail.com>
2024-07-29 16:04:28 -0300

    More descriptive about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,8 @@
-<p>The official Uiuisms page has been removed, and as such, this page should be used as a replacement.</p>
-<p>You can contribute to this page at <a href="https://github.com/JoaoFelipe3/uiuisms">the GitHub repository</a>.</p>
+<p>The official Uiuisms page has been removed, and as such, this page can be used as a replacement.</p>
+<p>
+    You can contribute to this page at <a href="https://github.com/JoaoFelipe3/uiuisms">the GitHub repository</a>,
+    or to <a href="https://github.com/JoaoFelipe3/uiuisms/edit/master/src/lib/data.js">this list</a>.
+</p>
 
 <svelte:head>
     <title>About Unofficial Uiuisms</title>
